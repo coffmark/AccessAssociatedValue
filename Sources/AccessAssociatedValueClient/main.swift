02abf9b2b8bcb49db0404c8330421993020517a0
coffmark <come1234560@gmail.com>
2023-12-10 17:16:23 +0900

    Create GitHub Actions workflow to build and test

--- Sources/AccessAssociatedValueClient/main.swift
+++ Sources/AccessAssociatedValueClient/main.swift
@@ -8,11 +8,11 @@ enum ViewState {
 }
 
 let viewStateInLoading = ViewState.loading(hasIndicator: true)
-print(viewStateInLoading.loading) // Optional(true)
-print(viewStateInLoading.loaded) // nil
+print(viewStateInLoading.loading ?? "nil") // Optional(true)
+print(viewStateInLoading.loaded ?? "nil") // nil
 
 let viewStateInLoaded = ViewState.loaded(cards: ["Card1", "Card2"], hasIndicator: false)
-print(viewStateInLoaded.loaded) // Optional((cards: ["Card1", "Card2"], hasIndicator: false))
-print(viewStateInLoaded.loaded?.cards) // Optional(["Card1", "Card2"])
-print(viewStateInLoaded.loading) // nil
+print(viewStateInLoaded.loaded ?? "nil") // Optional((cards: ["Card1", "Card2"], hasIndicator: false))
+print(viewStateInLoaded.loaded?.cards ?? "nil") // Optional(["Card1", "Card2"])
+print(viewStateInLoaded.loading ?? "nil") // nil
 
